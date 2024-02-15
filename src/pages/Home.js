4bb4d1932a2ac9b1d6086c348e9b4b7f0702d8e6
Adamya Singh <adamyasingh22@gmail.com>
2024-02-15 23:45:01 +0530

    Footer

---
+++ src/pages/Home.js
@@ -0,0 +1,12 @@
+import Footer from "../components/Footer"
+import FooterMade from "../components/Footer/FooterMade"
+
+const Home = ()=> {
+    return (
+        <div>
+           <Footer/>
+           <FooterMade/>
+        </div>
+    )
+}
+export default Home;
