4e5d189c60f70e987a3a5d985a6678418d52f5f5
Adamya Singh <adamyasingh22@gmail.com>
2024-02-16 00:10:34 +0530

    assests addition

---
+++ src/components/AssetsExport.js
@@ -0,0 +1,87 @@
+import Blob1 from "../assets/images/blob1.svg";
+import Blob2 from "../assets/images/blob2.svg";
+import Blob3 from "../assets/images/blob3.svg";
+import LeavesGreen from "../assets/images/leavesGreen.svg";
+import LeavesPink from "../assets/images/leavesPink.svg";
+import LogoGreen from "../assets/images/logoGreen.svg";
+import LogoMain from "../assets/images/logoMain.svg";
+import LogoWhite from "../assets/images/logoWhite.svg";
+import PrizeLines from "../assets/images/prizeLines.svg";
+import ScrollIcon from "../assets/images/scrollIcon.svg";
+import Starfish from "../assets/images/starfish.svg";
+import StarfishYellow from "../assets/images/starfishYellow.svg";
+import Weed1 from "../assets/images/weed1.svg";
+import Weed1Black from "../assets/images/weed1Black.svg";
+import Weed1Lime from "../assets/images/weed1Lime.svg";
+import Weed1Yellow from "../assets/images/weed1Yellow.svg";
+import Weed2 from "../assets/images/weed2.svg";
+import FirstPrize from "../assets/images/firstPrize.png";
+import SecondPrize from "../assets/images/secondPrize.png";
+import ThirdPrize from "../assets/images/thirdPrize.png";
+import MmilLogo from "../assets/images/mmilLogo.png";
+import Submarine from "../assets/images/submarine.png";
+import paytm from "../assets/images/paytm.svg";
+import gpay from "../assets/images/gpay.svg";
+import scantopay from "../assets/images/scantopay.svg";
+import DefaultUSer from "../assets/images/default_user.webp";
+
+import Discord from "../assets/icons/discord.svg";
+import Github from "../assets/icons/github.svg";
+import Linkedin from "../assets/icons/linkedIn.svg";
+import Mail from "../assets/icons/mail.svg";
+import Arrow from "../assets/icons/arrow.svg";
+import Hamburger from "../assets/icons/hamburger.svg";
+import Cross from "../assets/icons/cross.svg";
+import Refresh from "../assets/icons/refresh.svg";
+import Coin from "../assets/icons/coin.png";
+import Qr from "../assets/icons/qr.svg";
+import Success from "../assets/icons/success.png";
+import Error from "../assets/icons/error.png";
+import Verified from "../assets/icons/verified.png";
+import Status from "../assets/icons/status.svg";
+
+import loaderAnimationData from "../assets/lottie/loader.json";
+
+export {
+    Blob1,
+    Blob2,
+    Blob3,
+    LeavesGreen,
+    LeavesPink,
+    loaderAnimationData,
+    LogoGreen,
+    LogoMain,
+    LogoWhite,
+    PrizeLines,
+    Refresh,
+    ScrollIcon,
+    Starfish,
+    StarfishYellow,
+    Weed1,
+    Weed1Black,
+    Weed1Lime,
+    Weed1Yellow,
+    Weed2,
+    FirstPrize,
+    SecondPrize,
+    ThirdPrize,
+    MmilLogo,
+    Submarine,
+    Discord,
+    Github,
+    Linkedin,
+    Mail,
+    Arrow,
+    Hamburger,
+    Cross,
+    paytm,
+    gpay,
+    scantopay,
+    DefaultUSer,
+    Coin,
+    Qr,
+    Success,
+    Error,
+    Verified,
+    Status,
+};
---
+++ src/components/TitleDash.js
@@ -0,0 +1,26 @@
+const TitleDash = ({
+    title = "Hello",
+    isTop,
+    isYellow,
+    className = "",
+    ...props
+  }) => {
+    return (
+      <p
+        className={`relative tracking-widest text-md md:text-lg lg:text-2xl text-white font-bold md:font-black uppercase min-w-max
+            before:absolute 
+            ${!isTop && "before:-bottom-[8px] before:md:-bottom-[12px]"} 
+            ${isTop && "before:-top-[12px] before:md:-top-[18px]"} 
+            before:w-[35px] md:w-[48px] before:h-[6px] before:md:h-[9px] ${
+              isYellow ? "before:bg-yellow" : "before:bg-hot-pink"
+            } before:rounded ${className}
+           `}
+        {...props}
+      >
+        {title}
+      </p>
+    );
+  };
+  
+  export default TitleDash;
+  
