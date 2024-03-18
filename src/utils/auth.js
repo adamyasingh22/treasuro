80298bcb189dfc5976d74b3931e2352665928c0e
Adamya Singh <adamyasingh22@gmail.com>
2024-03-18 20:00:53 +0530

    utils

---
+++ src/utils/auth.js
@@ -0,0 +1,44 @@
+const TOKEN_KEY = "token";
+
+export const auth = {
+	isAuthenticated,
+	getToken,
+	logout,
+	getUser,
+	isUserAvailable,
+	setUser, 
+	setToken
+};
+
+function getToken() {
+	return JSON.parse(localStorage.getItem(TOKEN_KEY));
+}
+
+function getUser() {
+	return JSON.parse(localStorage.getItem("user"));
+}
+
+function setToken(token) {
+	localStorage.setItem(TOKEN_KEY, token)
+}
+
+function setUser(user) {
+	localStorage.setItem("user", user)
+}
+
+function isAuthenticated() {
+	return !!getToken();
+}
+
+function isUserAvailable(){
+	return !!localStorage.getItem("user");
+}
+
+async function logout() {
+	try {
+		localStorage.clear();
+		return {
+			success: true,
+		};
+	} catch (err) {}
+}
