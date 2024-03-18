0c372a4ba3382b087413e26d286fb4d5b76c5b59
Adamya Singh <adamyasingh22@gmail.com>
2024-03-18 20:49:55 +0530

    config

---
+++ src/utils/config.js
@@ -0,0 +1,67 @@
+import axios from "axios";
+import Cookies from "js-cookie";
+
+const customAxios = axios.create({
+	baseURL: ``,
+	timeout: 10000,
+});
+
+customAxios.interceptors.request.use(
+	(config) => {
+		const token = Cookies.get("access-token");
+		if (token) {
+			config.headers["Authorization"] = "Bearer " + token;
+		}
+		// config.headers['Content-Type'] = 'application/json';
+		return config;
+	},
+	(error) => {
+		Promise.reject(error);
+	}
+);
+
+customAxios.interceptors.response.use(
+	(response) => {
+		return response;
+	},
+	function (error) {
+		const originalRequest = error.config;
+
+		if (
+			error.response.status === 401 &&
+			originalRequest.url === "https://treasuro.in/api/auth/refresh"
+		) {
+			return Promise.reject(error);
+		}
+
+		if (error.response.status === 403 && !originalRequest._retry) {
+			originalRequest._retry = true;
+			const refreshToken = Cookies.get("refresh-token");
+			return axios
+				.post("https://treasuro.in/api/auth/refresh", {
+					refresh: refreshToken,
+				})
+				.then((res) => {
+					if (res.status === 403) {
+						return Promise.reject(res);
+					}
+					if (res.status === 200) {
+						Cookies.set("refresh-token", res.data);
+						axios.defaults.headers.common["Authorization"] =
+							"Bearer " + Cookies.get("access-token");
+						return axios(originalRequest);
+					}
+				});
+		}
+		return Promise.reject(error);
+	}
+);
+
+customAxios.interceptors.response.use(
+	(res) => res,
+	(err) => {
+		throw new Error(err.response.data.message);
+	}
+);
+
+export default customAxios;
